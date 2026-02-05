a29ead5328ed22eff0b6e150d3fe681c49ea156d
GajuOSCP <97144423+GajuOSCP@users.noreply.github.com>
2026-02-05 15:09:15 +0530

    Update CorsConfig.java

--- src/main/java/com/example/Gpseva/config/CorsConfig.java
+++ src/main/java/com/example/Gpseva/config/CorsConfig.java
@@ -17,17 +17,17 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
 
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
     }
 }
+
 
 
 
